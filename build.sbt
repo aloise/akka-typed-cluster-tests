b04269c4cdcc799478ef88906ff96ae86094b952
Ihor <erwaht@gmail.com>
2019-09-15 19:12:57 +0200

    application setup

--- build.sbt
+++ build.sbt
@@ -6,5 +6,10 @@ scalaVersion := "2.13.0"
 
 libraryDependencies ++= Seq(
   "com.typesafe.akka" %% "akka-actor-typed" % Version.akka,
-  "com.typesafe.akka" %% "akka-distributed-data" % Version.akka
+  "com.typesafe.akka" %% "akka-cluster-typed" % Version.akka,
+  "com.typesafe.akka" %% "akka-slf4j" % Version.akka,
+  "com.typesafe.akka" %% "akka-distributed-data" % Version.akka,
+  "ch.qos.logback" % "logback-classic" % "1.2.3"
 )
+// Workaround - https://github.com/sbt/sbt/issues/5075
+classLoaderLayeringStrategy := ClassLoaderLayeringStrategy.AllLibraryJars
--- src/main/scala/name/aloise/Main.scala
+++ src/main/scala/name/aloise/Main.scala
@@ -1,11 +1,22 @@
 package name.aloise
 
-import java.util.UUID
-import akka.actor.typed.scaladsl.Behaviors
-import akka.actor.typed.{ActorRef, ActorSystem, Behavior, DispatcherSelector, Terminated}
+import akka.actor.typed.ActorSystem
+import name.aloise.actors.RegistryActor
+import name.aloise.actors.RegistryActor.RegistryMessage
+
+import scala.io.StdIn
 
 object Main extends App {
 
-  //  val system = ActorSystem("default")
+  val system: ActorSystem[RegistryMessage] = ActorSystem(RegistryActor.main("Main"), "default")
+
+  Console.println("Running the cluster node ...")
+  Console.println("Press <enter> to stop")
+
+  StdIn.readLine()
+
+  system.terminate()
+
+
 }
 
